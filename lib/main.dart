d6145bd30c653fdf60cb3fab557bdf588ab15075
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-08 18:41:17 -0300

    Criando Classe Transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,26 @@ import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: const [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.00'),
-              subtitle: Text('3582'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.00'),
-              subtitle: Text('3582'),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencias(),
       appBar: AppBar(
-          centerTitle: true,
-          toolbarHeight: 88,
-          backgroundColor: const Color(0xff198314),
-          title: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "images/logo.png",
-                  fit: BoxFit.contain,
-                  height: 52,
-                ),
-                Text("Transferências", style: GoogleFonts.berkshireSwash(),)
-              ])),
+        centerTitle: true,
+        toolbarHeight: 88,
+        backgroundColor: const Color(0xff198314),
+        title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "images/logo.png",
+                fit: BoxFit.contain,
+                height: 52,
+              ),
+              Text(
+                "Transferências",
+                style: GoogleFonts.berkshireSwash(),
+              )
+            ]),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
@@ -44,4 +31,45 @@ void main() {
       ),
     ),
   ));
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Column(
+        children:  [
+          ItemTransferencia(Transferencia(100,3582)),
+          ItemTransferencia(Transferencia(200,3583)),
+          ItemTransferencia(Transferencia(300,3584)),
+        ],
+      );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+
 }
