f186b36b809f03d5f0c06812c4e1d093b95acb64
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-08 18:08:22 -0300

    Card de Transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Scaffold(
+    home: Scaffold(
+      body: Column(
+        children: const [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.00'),
+              subtitle: Text('3582'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.00'),
+              subtitle: Text('3582'),
+            ),
+          ),
+        ],
+      ),
       appBar: AppBar(
-        centerTitle: true,
-        title: Image.asset(
-          "images/logo.png",
-          fit: BoxFit.contain,
-          height: 72,
-        ),
-        toolbarHeight: 88,
-        backgroundColor: const Color(0xff198314),
-        ),
+          centerTitle: true,
+          toolbarHeight: 88,
+          backgroundColor: const Color(0xff198314),
+          title: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "images/logo.png",
+                  fit: BoxFit.contain,
+                  height: 52,
+                ),
+                Text("Transferências", style: GoogleFonts.berkshireSwash(),)
+              ])),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
         backgroundColor: const Color(0xff198314),
-    ),
       ),
-
-    )
-  );
+    ),
+  ));
 }
