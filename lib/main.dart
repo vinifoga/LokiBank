7a1f88b9e68b599f194f52e637a9779a82dcff33
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-08 19:07:53 -0300

    Criando Formulario Transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: ListaTransferencias(),
-      appBar: AppBar(
-        centerTitle: true,
-        toolbarHeight: 88,
-        backgroundColor: const Color(0xff198314),
-        title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                "images/logo.png",
-                fit: BoxFit.contain,
-                height: 52,
-              ),
-              Text(
-                "Transferências",
-                style: GoogleFonts.berkshireSwash(),
-              )
-            ]),
+void main() => runApp(const LokiBankApp());
+
+class LokiBankApp extends StatelessWidget {
+  const LokiBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        appBar: AppBarLoki(
+          appBarTitle: "Criando Transferência",
+        ),
+        body: Text('Teste'));
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarLoki(appBarTitle: "Transferências"),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(100, 3582)),
+          ItemTransferencia(Transferencia(200, 3583)),
+          ItemTransferencia(Transferencia(300, 3584)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
         backgroundColor: const Color(0xff198314),
       ),
-    ),
-  ));
-}
-
-class ListaTransferencias extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return
-      Column(
-        children:  [
-          ItemTransferencia(Transferencia(100,3582)),
-          ItemTransferencia(Transferencia(200,3583)),
-          ItemTransferencia(Transferencia(300,3584)),
-        ],
-      );
+    );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   const ItemTransferencia(this._transferencia);
@@ -70,6 +70,36 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+}
 
+class AppBarLoki extends StatelessWidget implements PreferredSizeWidget {
+  final String appBarTitle;
 
+  const AppBarLoki({Key? key, required this.appBarTitle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      toolbarHeight: 88,
+      backgroundColor: const Color(0xff198314),
+      title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/logo.png",
+              fit: BoxFit.contain,
+              height: 52,
+            ),
+            Text(
+              appBarTitle,
+              style: GoogleFonts.berkshireSwash(),
+            )
+          ]),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
 }
